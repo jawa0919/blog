bc08c516bb7d010dc4ba743cf9714cb0f567a706
jawa0919 <jawa0919@163.com>
2025-12-01 14:46:23 +0800

    sdkHome

--- script/add_env.ps1
+++ script/add_env.ps1
@@ -8,7 +8,6 @@ Get-ChildItem Env:
 # 这个路径一般是C:\Users\Administrator
 $USP = [Environment]::GetEnvironmentVariable('USERPROFILE')
 $sdkHome = "D:\.sdkHome"
-$sdkCache = "D:\.sdkCache"
 # Java
 Write-Host "Set Java Environment" -ForegroundColor Green
 [Environment]::SetEnvironmentVariable('JAVA_HOME', "C:\Program Files\Android\Android Studio\jbr", 'User')
@@ -17,13 +16,13 @@ Write-Host "Set Java Environment" -ForegroundColor Green
 Write-Host "Set Android Environment" -ForegroundColor Green
 [Environment]::SetEnvironmentVariable('ANDROID_HOME', $sdkHome + "\AndroidSdk" , 'User')
 [Environment]::SetEnvironmentVariable('ANDROID_SDK_ROOT',  $sdkHome + "\AndroidSdk" , 'User')
-[Environment]::SetEnvironmentVariable('GRADLE_USER_HOME',  $sdkCache + "\.gradle" , 'User')
+[Environment]::SetEnvironmentVariable('GRADLE_USER_HOME',  $sdkHome + "\.gradle" , 'User')
 # Flutter
 Write-Host "Set Flutter Environment" -ForegroundColor Green
 [Environment]::SetEnvironmentVariable('PUB_HOSTED_URL', "https://pub.flutter-io.cn" , 'User')
 [Environment]::SetEnvironmentVariable('FLUTTER_STORAGE_BASE_URL', "https://storage.flutter-io.cn" , 'User')
 [Environment]::SetEnvironmentVariable('FLUTTER_SDK_ROOT', $sdkHome + "\FlutterSdk\flutter3_27_4" , 'User')
-[Environment]::SetEnvironmentVariable('PUB_CACHE', $sdkCache + "\.pub-cache" , 'User')
+[Environment]::SetEnvironmentVariable('PUB_CACHE', $sdkHome + "\.pub-cache" , 'User')
 # Path
 Write-Host "Add Path" -ForegroundColor Green
 $sdk_path = "%JAVA_HOME%\bin;%ANDROID_SDK_ROOT%\tools;%ANDROID_SDK_ROOT%\platform-tools;%FLUTTER_SDK_ROOT%\bin;%PUB_CACHE%\bin;"
